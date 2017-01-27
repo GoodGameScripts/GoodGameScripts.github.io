b0c0db8b5708e713ace9fb87e90a23f47399b9ee
GoodGameScripts <GoodGameScripts@users.noreply.github.com>
2017-01-27 13:47:02 -0800

    Site updated to

---
+++ javascripts/search.js
@@ -0,0 +1,57 @@
+$( window ).ready(function() {
+
+  var WidthInPixels = $('.SWFWrapper').outerWidth(true)*.8;
+  var input = document.getElementById("search");
+  var awesomplete = new Awesomplete(input, {
+    minChars: 1,
+    maxItems: 5,
+    autoFirst: true
+  });
+  window.addEventListener("awesomplete-selectcomplete", function(e) {
+
+    var articleTitle = $(e.target).val().replace(/ /g,'');
+    var articleId = '#'+ articleTitle;
+    var $article = $(articleId);
+    var articleUrl = $article.data('link');
+    window.location.href = articleUrl;
+    });
+
+
+$( "#minimize" ).click(function() {
+    $( "aside" ).toggleClass("hideaside")
+    $("#minimizer").toggleClass("fa-angle-double-right");
+    $("#fixed").toggleClass("fixed");
+    $("#rightside").toggleClass("articleOnhide");
+    $("body").toggleClass("hidesidenav");
+    var $el = $(this);
+   var title = $(this).prop('title');
+
+   if(title === 'Minimize Sidebar'){
+     // Reset state
+     $el.prop('title', 'Maximize Sidebar');
+   } else {
+     $el.prop('title', 'Minimize Sidebar');
+   }
+
+
+});
+
+
+$(".swf").css("height", WidthInPixels + 'px');
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
+
+});
